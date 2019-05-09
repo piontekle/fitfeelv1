9568651d776a1a93c886800bb6977be8f1b19dac
Lauren Piontek <piontekle@gmail.com>
2019-05-09 11:08:11 -0400

    static file path corrected

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -15,13 +15,13 @@ module.exports = {
     app.use(express.static(path.join(__dirname, 'client/build')));
     if(process.env.NODE_ENV === 'production') {
       app.use(express.static(path.join(__dirname, 'client/build')));
-      
+
       app.get('*', (req, res) => {
         res.sendfile(path.join(__dirname = 'client/build/index.html'));
       })
     }
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname+'/client/public/index.html'));
+      res.sendFile(path.join(__dirname+'../client/public/index.html'));
     })
     app.use(cors());
     app.use(morgan("dev"));
