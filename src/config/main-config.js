6367b03d66c5965db697451f91d10b31230db450
Lauren Piontek <piontekle@gmail.com>
2019-05-09 09:05:19 -0400

    file config for deployment

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -12,7 +12,17 @@ module.exports = {
   init(app){
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
-    app.use(express.static(path.join(__dirname, "client/build")))
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    if(process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, 'client/build')));
+      //
+      app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = 'client/build/index.html'));
+      })
+    }
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname+'/client/public/index.html'));
+    })
     app.use(cors());
     app.use(morgan("dev"));
     app.use(expressValidator());
