1dad5067e414f38c7da55315ffff57d9c8f00aee
Lauren Piontek <piontekle@gmail.com>
2019-06-13 17:16:25 -0400

    express static without dirname

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -23,7 +23,7 @@ module.exports = {
     })
     app.use(express.static(path.join(__dirname, '../..', 'client/build')));
     if(process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join(__dirname, '../..', 'client/build')));
+      app.use(express.static(path.join('client/build')));
 
       app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../..', 'client/build/index.html'));
