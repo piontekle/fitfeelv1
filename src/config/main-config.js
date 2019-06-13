ba3bb0d1a4dac71315f4e0a249ae85b0496e02e5
Lauren Piontek <piontekle@gmail.com>
2019-06-13 16:24:10 -0400

    sendFile path fixed

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -26,7 +26,7 @@ module.exports = {
       app.use(express.static(path.join(__dirname, '../..', 'client/build')));
 
       app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + 'client/build/index.html'));
+        res.sendFile(path.join(__dirname, '../..' 'client/build/index.html'));
       })
     }
     app.use(cors());
