9dea57e40a3db78d99bf41eafc3edb3bae06d0b1
Lauren Piontek <piontekle@gmail.com>
2019-06-13 17:21:58 -0400

    move app.get * out of if

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -23,12 +23,11 @@ module.exports = {
     })
     app.use(express.static(path.join(__dirname, '../..', 'client/build')));
     if(process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join('client/build')));
-
-      app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../..', 'client/build/index.html'));
-      })
+      app.use(express.static(path.join(__dirname, '../..', 'client/build')));
     }
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../..', 'client/build/index.html'));
+    })
     app.use(cors());
     app.use(morgan("dev"));
     app.use(expressValidator());
