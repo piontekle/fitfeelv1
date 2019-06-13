56c620dee1d6b1302f09af0c07e8b50cb2495832
Lauren Piontek <piontekle@gmail.com>
2019-06-13 17:28:32 -0400

    remove wildcard

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -25,9 +25,6 @@ module.exports = {
     if(process.env.NODE_ENV === 'production') {
       app.use(express.static(path.join(__dirname, '../..', 'client/build')));
     }
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../..', 'client/build/index.html'));
-    })
     app.use(cors());
     app.use(morgan("dev"));
     app.use(expressValidator());
