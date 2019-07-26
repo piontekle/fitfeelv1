20a66b3cf03e4ef66914bd759e42e660d3997a9a
Lauren Piontek <piontekle@gmail.com>
2019-07-25 22:21:44 -0400

    Update CSS, add about images

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import brain from '../images/strongbrain.jpg';
+import feelForm from '../images/feelform.jpg';
 
 class About extends Component {
 
@@ -9,13 +11,15 @@ class About extends Component {
       <section id="about" className="mdl-grid">
           <div className="mdl-cell mdl-cell--8-col">
             <h1 id="about-h">About FitFeel</h1>
-            <p>Obviously taking care of yourself physically is important to you, but are you keeping yourself mentally fit as well?  With FitFeel, you can "check-in" with yourself before and after workouts. Did you sleep well? Are deadlines coming up? Are you excited for the concert next week? Take a minute to think about where you are mentally before starting a workout and how you're feeling after.[picture of brain?]</p>
+            <p>Obviously taking care of yourself physically is important to you, but are you keeping yourself mentally fit as well?  With FitFeel, you can "check-in" with yourself before and after workouts. Did you sleep well? Are deadlines coming up? Are you excited for the concert next week? Take a minute to think about where you are mentally before starting a workout and how you're feeling after.</p>
+            <p><img src={brain} alt="strong-brain" /></p>
           </div>
         </section>
         <section id="how" className="mdl-grid">
           <div className="mdl-cell mdl-cell--8-col">
             <h4 id="how-to-h">How?</h4>
-            <p>Before each workout, open FitFeel to answer a few quick questions about where you are mentally. You can simply check the boxes or elaborate - you can put as much time as you want or have into it. [picture of form] Then after your workout, check-in again to see how things have changed.[picture of form]</p>
+            <p>Before each workout, open FitFeel to answer a few quick questions about where you are mentally. You can simply check the boxes or elaborate - you can put as much time as you want or have into it. <img src={feelForm} id="feelFormImg" alt="check-in-form" /></p>
+            <p>Then after your workout, check-in again to see how things have changed.</p>
           </div>
         </section>
       </section>
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -6,9 +6,9 @@ render() {
       if(!this.props.loggedIn) {
         return (
         <div id="overview">
-          <section className="section--center mdl-grid mdl-card mdl-shadow--2dp">
+          <section className="section--center mdl-grid mdl-card mdl-shadow--2dp" id="welcome-card">
             <div className="mdl-card mdl-cell mdl-cell--12-col">
-              <div className="mdl-card__supporting-text ">
+              <div className="mdl-card__supporting-text">
                 <h4 className="mdl-cell mdl-cell--12-col">Welcome to FitFeel</h4>
                 <div className="section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone">
                   An app to check-in mentally before & after workouts to keep your mind just as fit as your body.
@@ -22,14 +22,14 @@ render() {
               </div>
             </div>
           </section>
-          <section className="section--footer mdl-color--white mdl-grid">
+          <footer className="section--footer mdl-color--white mdl-grid">
               <div className="section__text mdl-cell mdl-cell--4-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone">
                 <h5 className="mdl-cell mdl-cell--12-col">Users</h5>
               </div>
               <div className="mdl-card__actions">
                 <button className="mdl-button"><Link to={'./sign-in'}>Sign In</Link></button>
               </div>
-          </section>
+          </footer>
         </div>
       );
     }
